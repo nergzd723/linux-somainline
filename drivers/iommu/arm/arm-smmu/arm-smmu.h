fb0d0177168c36050e8bed4b0267a209ccf3ecb0
AngeloGioacchino Del Regno <kholk11@gmail.com>
2020-09-24 00:36:51 +0200

    iommu/arm-smmu: Support test_smr_masks implementation detail deviation
    At least some Qualcomm SoCs do need to override the function
    arm_smmu_test_smr_masks entirely: add a test_smr_masks function
    to the implementation details structure and call it properly.
    
    Signed-off-by: AngeloGioacchino Del Regno <kholk11@gmail.com>

--- drivers/iommu/arm/arm-smmu/arm-smmu.h
+++ drivers/iommu/arm/arm-smmu/arm-smmu.h
@@ -387,6 +387,7 @@ struct arm_smmu_impl {
 	int (*cfg_probe)(struct arm_smmu_device *smmu);
 	int (*reset)(struct arm_smmu_device *smmu);
 	int (*init_context)(struct arm_smmu_domain *smmu_domain);
+	void (*test_smr_masks)(struct arm_smmu_device *smmu);
 	void (*tlb_sync)(struct arm_smmu_device *smmu, int page, int sync,
 			 int status);
 	int (*def_domain_type)(struct device *dev);
